8e233e58f12b62efdf1e30fcf6bed34401f97e56
Antti Valli <antti.valli@gmail.com>
2022-10-18 08:39:39 +0300

    Function to move cursor

--- hello.s
+++ hello.s
@@ -2,6 +2,67 @@ global _start
 
 
 section .text
+
+; write a two digit number in a buffer
+; rdi is the adress of a buffer
+; rsi is the number to format
+; returns the number of digits in rax 
+numForm:
+  mov rax, rsi
+  mov r11, rdi
+  cqo
+  mov rcx, 10
+  div rcx ; rax = rax/10, rdx = rax%10
+  test rax, rax
+  je numForm_skipTens
+  mov byte [rdi], '0'
+  add byte [rdi], al
+  inc rdi
+numForm_skipTens:
+  mov byte [rdi], '0'
+  add byte [rdi], dl
+  inc rdi
+  mov rax, rdi
+  sub rax, r11 
+  ret  
+
+;move cursor to coords given in rdi, rsi
+moveCursor:
+  inc rdi              ; Make the origin (0,0)
+  inc rsi
+  push rbp             ; callee saved registeris, pushing also aligns the stack
+  push r12
+  push r13
+  mov r12, rdi         ; Make a backup of the second argument
+  mov rbp, msgBuff
+  mov byte [rbp], 0o33
+  inc rbp
+  mov byte [rbp], '['
+  inc rbp
+  mov rsi, rdi         ; the second argument is the first argument of this function
+  mov rdi, rbp         ; the first argument is the buffer
+  call numForm
+  add rbp, rax         ; progress the pointer by the number of digits
+
+  mov byte [rbp], ';'
+  inc rbp
+  mov rsi, r12         ; the second argument is the second argument of this function
+  mov rdi, rbp         ; the first argument is the buffer
+  call numForm
+  add rbp, rax         ; progress the pointer by the number of digits
+  mov byte [rbp], 'H'
+  inc rbp
+  mov rax, 1
+  mov rdi, 1
+  mov rsi, msgBuff 
+  mov rdx, rbp
+  sub rdx, rsi
+  syscall
+  pop r13
+  pop r12
+  pop rbp
+  ret
+
 clearScreen:
   mov rax, 1
   mov rdi, 1
@@ -43,11 +104,11 @@ _start:
   call clearScreen
   mov rbx, 10
 loop:
-; mov rax, 1       ; write(
-;  mov rdi, 1       ; stdout,
+  lea rsi, [2*rbx]
+  mov rdi, rbx
+  call goToCol
   mov rsi, msg     ; "Hello world!\n"
   mov rdx, msglen  ; sizeof(msg)
-;  syscall          ; );
   call bluePrint
   dec rbx
   jnz loop
@@ -57,7 +118,10 @@ loop:
   mov rdi, 0       ;  EXIT_SUCCESS
   syscall          ; );
 
+section .bss
+  msgBuff: resb   64 ; 64 byte buffer   
 section .rodata
+  strCUP: db 0o33, "[5;5H"
   blue: db 0o33, "[94m"
   bluelen: equ $ - blue
   defaultcolor: db 0o33, "[0m"
